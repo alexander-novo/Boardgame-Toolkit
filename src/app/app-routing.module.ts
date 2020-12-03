245abd83c97df578b70197c6afd4a186cebf4154
Alexander Novotny <alexander_novo@mail.tmcc.edu>
2020-12-02 16:22:44 -0800

    Started working on login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 const routes: Routes = [];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
-	exports: [RouterModule, MatCardModule, MatButtonModule]
+	exports: [RouterModule, MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule]
 })
 export class AppRoutingModule { }
