0a5ee35a3626b12ea8a0816cda90ade8bc912d95
Alexander Novotny <alexander_novo@mail.tmcc.edu>
2020-12-07 09:54:10 -0800

    Fixed a bug with server.js
    Issue was with referencing a variable that hadn't been initialised yet

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ var jwt = require('jsonwebtoken');
 const User = require("./schemas/user");
 const Project = require("./schemas/project");
 
-// Since we're using cookie-parser middleware, cookies should be added to req.cookies.
-// In /api/login and /api/register, we attached the auth token as the 'jwt' cookie.
-const EJWT_OPTIONS = { secret: jwtSecret, algorithms: ['HS512'], getToken: (req) => req.cookies.jwt };
-const GEN_JWT = (user) => jwt.sign({ id: user.id }, jwtSecret, { expiresIn: "7d", algorithm: "HS512" });
-
 var app = express();                        // create our app w/ express
 var port = process.env.PORT || 8888;
 var dbUri = process.env.MONGODB_URI || "mongodb://localhost/TestDB";
 var jwtSecret = process.env.JWT_SECRET || "keyboard cat";
+
+// Since we're using cookie-parser middleware, cookies should be added to req.cookies.
+// In /api/login and /api/register, we attached the auth token as the 'jwt' cookie.
+const EJWT_OPTIONS = { secret: jwtSecret, algorithms: ['HS512'], getToken: (req) => req.cookies.jwt };
+const GEN_JWT = (user) => jwt.sign({ id: user.id }, jwtSecret, { expiresIn: "7d", algorithm: "HS512" });
 
 mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(() => {
 	console.log("Connected to database!");
